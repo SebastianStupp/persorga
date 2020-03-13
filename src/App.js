a966a4f9ab0a3cb513dffb7aed7eb5a6f1b07352
SebastianStupp <s.stupp@outlook.de>
2020-03-13 11:30:33 +0100

    Add Basic Html Structure / Add Routing for Pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import LoginFailed from "./components/LoginFailed";
+import MemberList from "./components/MemberList";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/Login">
+          <Login></Login>
+        </Route>
+        <Route path="/LoginFailed">
+          <LoginFailed></LoginFailed>
+        </Route>
+        <Route path="/MemberList">
+          <MemberList></MemberList>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function loginScreen() {
+  return (
+    <form className="loginScreen">
+      <input class="inputName"></input>
+      <input class="inputPassword"></input>
+    </form>
+  );
+}
+
+export default loginScreen;
