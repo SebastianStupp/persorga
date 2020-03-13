7a68c2b49b10f0da61e7a20e3b091f80d20493af
SebastianStupp <s.stupp@outlook.de>
2020-03-13 17:07:31 +0100

    Add json fetch on Click

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,40 @@
 import React from "react";
 import "./Login.css";
 
-function loginScreen() {
+function LoginScreen() {
+  const [loginName, setLoginName] = React.useState("");
+  const [loginPassword, setLoginPassword] = React.useState("");
+
+  async function handleLoginClick(event) {
+    event.preventDefault();
+
+    const memberList = await fetch("http://localhost:4000/member");
+    const response = await memberList.json();
+    const result = await response;
+    console.log(result);
+  }
+
   return (
     <form className="loginScreen">
-      <input className="inputName" placeholder="Enter User Name"></input>
-      <input className="inputPassword" placeholder="Enter Password"></input>
-      <button className="loginButton">Login</button>
+      <input
+        className="inputName"
+        placeholder="Enter User Name"
+        onChange={event => {
+          setLoginName(event.target.value);
+        }}
+      ></input>
+      <input
+        className="inputPassword"
+        placeholder="Enter Password"
+        onChange={event => {
+          setLoginPassword(event.target.value);
+        }}
+      ></input>
+      <button className="loginButton" onClick={handleLoginClick}>
+        Login
+      </button>
     </form>
   );
 }
 
-export default loginScreen;
+export default LoginScreen;
