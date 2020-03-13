0622898400f6791d2e3363d19cbc86977064d410
SebastianStupp <s.stupp@outlook.de>
2020-03-13 15:51:33 +0100

    Add Routing for Try Again Button / Create Login Button and styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,7 @@ function loginScreen() {
     <form className="loginScreen">
       <input className="inputName" placeholder="Enter User Name"></input>
       <input className="inputPassword" placeholder="Enter Password"></input>
+      <button className="loginButton">Login</button>
     </form>
   );
 }
--- src/components/LoginFailed.js
+++ src/components/LoginFailed.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./LoginFailed.css";
+import { useHistory } from "react-router-dom";
 
 function LoginFailed() {
+  let history = useHistory();
+
+  function handleClick() {
+    history.push("/");
+  }
   return (
     <div className="loginFailed">
       <h1>Sorry Login Failed 🥺</h1>
       <h2>Please Try Again or Contact Support</h2>
-      <button className="buttonBack">Try Again</button>
+
+      <button className="buttonBack" onClick={handleClick}>
+        Try Again
+      </button>
     </div>
   );
 }
+
 export default LoginFailed;
